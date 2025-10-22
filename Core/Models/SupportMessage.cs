d78df2f1b2884ed3ad62ca6cc953d60b00ff040f
Hjalte <hjaltebording53@gmail.com>
2025-10-22 17:56:45 +0200

    start

---
+++ Core/Models/SupportMessage.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography;
+
+namespace Core.Models;
+
+public class SupportMessage
+{
+    public int Id { get; set; }
+    
+    [Required]
+    public string Name { get; set; }
+    
+    [Required]
+    public string Email { get; set; }
+    
+    public string Phone { get; set; }
+    
+    [Required]
+    public string Description { get; set; }
+    
+    [Required]
+    public string Category { get; set; }
+    
+    public DateTime Date { get; set; }
+    
+}
